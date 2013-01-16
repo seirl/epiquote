3967359ff1707327998d5bf5b7cfe2ed37cc7fec
Antoine Pietri <antoine.pietri@epita.fr>
2013-01-16 03:45:41 +0000

    added page navigation

--- epiquote/urls.py
+++ epiquote/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     url(r'^last$', views.last_quotes),
     url(r'^last/(\d+)$', views.last_quotes),
     url(r'^random$', views.random_quotes),
-    url(r'^all$', views.all_quotes),
     url(r'^search$', views.search_quotes),
     url(r'^admin/', include(admin.site.urls)),
 )
--- quotes/views.py
+++ quotes/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django import forms
+from django.http import Http404, HttpResponseRedirect
 import shlex
+
+NUMBER_PER_PAGE = 30
 
 
 class SearchForm(forms.Form):
@@ -33,15 +36,24 @@ def get_quotes(only_visible=True, order='-date'):
     return quotes
 
 
-def split_quotes(quotes, page=0, number=30):
+def split_quotes(quotes, page=0, number=NUMBER_PER_PAGE):
     return quotes[page * number: (page + 1) * number]
 
 
-def last_quotes(request, page='0'):
+def last_quotes(request, page=0):
+    if 'p' in request.GET:
+        return HttpResponseRedirect('/last/{0}'.format(request.GET['p']))
     page = int(page)
-    quotes = split_quotes(get_quotes(), page=page)
-    return render(request, 'simple.html', {'name_page': 'Dernières citations',
-        'quotes': quotes})
+    all_quotes = get_quotes()
+    max_page = len(all_quotes) / NUMBER_PER_PAGE
+    if page < 0 or page > max_page:
+        raise Http404()
+    next_page = None if page >= max_page else page + 1
+    prev_page = None if page <= 0 else page - 1
+    quotes = split_quotes(all_quotes, page=page)
+    return render(request, 'last.html', {'name_page': 'Dernières citations',
+        'quotes': quotes, 'next_page': next_page, 'prev_page': prev_page,
+        'page': page, 'max_page': max_page})
 
 
 def random_quotes(request):
@@ -50,26 +62,23 @@ def random_quotes(request):
         'Citations aléatoires', 'quotes': quotes})
 
 
-def all_quotes(request):
-    quotes = get_quotes()
-    return render(request, 'simple.html', {'name_page':
-        'Toutes les citations', 'quotes': quotes})
-
-
 def search_quotes(request):
-    if 'q' not in request.GET:
-        raise Exception
-    else:
-        quotes = get_quotes()
-        query = map(lambda s: u' {0} '.format(s.decode('utf-8')),
-                shlex.split(request.GET['q'].encode('utf-8')))
-        for w in query:
-            f = (Q(content__icontains=w)
-               | Q(context__icontains=w)
-               | Q(author__icontains=w))
-            quotes = quotes.filter(f)
-        return render(request, 'simple.html', {'name_page':
-            u'Recherche : {0}'.format(request.GET['q']), 'quotes': quotes})
+    f = SearchForm(request.GET)
+    if not f.is_valid():
+        raise Http404()
+    query = f.cleaned_data['q']
+    quotes = get_quotes()
+    terms = map(lambda s: u' {0} '.format(s.decode('utf-8')),
+            shlex.split(query.encode('utf-8')))
+    for w in terms:
+        f = (Q(content__icontains=w)
+           | Q(context__icontains=w)
+           | Q(author__icontains=w))
+        quotes = quotes.filter(f)
+    if not quotes:
+        raise Http404()
+    return render(request, 'simple.html', {'name_page':
+        u'Recherche : {0}'.format(request.GET['q']), 'quotes': quotes})
 
 
 @login_required
