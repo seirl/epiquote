9882ea29f6ebbaae73a5362b58afed10c15c4bef
Antoine Pietri <antoine.pietri@epita.fr>
2013-01-20 22:04:49 +0000

    little heart for favourite

--- quotes/static/js/fav.js
+++ quotes/static/js/fav.js
@@ -1,11 +1,11 @@
 function fav(id) {
   $.post('/'+id+'/favourite', {HTTP_X_REQUESTED:'XMLHttpRequest'});
   var q = document.getElementById("f" + id);
-  if (q.innerHTML == 'Supprimer des favoris') {
-    q.innerHTML = 'Ajouter en favori';
+  if (q.innerHTML == '&lt;3') {
+    q.innerHTML = '&lt;/3';
   }
   else
   {
-    q.innerHTML = 'Supprimer des favoris';
+    q.innerHTML = '&lt;3';
   }
 }
