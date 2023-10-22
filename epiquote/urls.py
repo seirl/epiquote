b6e8739155d6bee2938b65cbd48beb75d3ee5e12
Antoine Pietri <antoine.pietri1@gmail.com>
2023-10-22 21:31:55 +0200

    Add crashtest view.

--- epiquote/urls.py
+++ epiquote/urls.py
@@ -1,9 +1,17 @@
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 from django_registration.backends.activation.views import RegistrationView
 
 from epiquote.forms import UserRegistrationForm
+
+
+def crash_test(request):
+    if request.user.is_staff:
+        raise RuntimeError("Crash test.")
+    return HttpResponse(status=404)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,6 +23,7 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('comments/', include('django_comments.urls')),
+    path('crashtest', crash_test),
     path('', include('quotes.urls')),
 ]
 
